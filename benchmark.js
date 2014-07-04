d255f42a9e3a337f8e3975cd751741feaed2fff9
Sindre Sorhus <sindresorhus@gmail.com>
2014-07-04 22:29:02 +0200

    bench - minor code style tweaks

--- benchmark.js
+++ benchmark.js
@@ -1,20 +1,15 @@
-// ran through matcha
-// ./node_modules/matcha/bin/matcha benchmark.js
+var chalk = require('./');
 
-var chalk = require('./index.js');
-
-suite('chalk', function(){
-
-	bench('add colour', function(){
+suite('chalk', function () {
+	bench('single style', function () {
 		chalk.red('the fox jumps over the lazy dog');
 	});
 
-	bench('add several styles', function(){
-		chalk.blue.bgRed.bold('the fox jumps over the lazy dog') ;
+	bench('several styles', function () {
+		chalk.blue.bgRed.bold('the fox jumps over the lazy dog');
 	});
 
-	bench('add nested styles', function(){
-		chalk.red('the fox jumps ', chalk.underline.bgBlue('over the lazy dog') + '!') ;
+	bench('nested styles', function () {
+		chalk.red('the fox jumps', chalk.underline.bgBlue('over the lazy dog') + '!');
 	});
-
 });
