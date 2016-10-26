492f11fb597094199c2e1926a73c64b94c759f06
Sindre Sorhus <sindresorhus@gmail.com>
2016-10-26 12:58:01 +0700

    add example file

---
+++ example.js
@@ -0,0 +1,17 @@
+'use strict';
+const chalk = require('./');
+
+// generates screenshot
+for (const key of Object.keys(chalk.styles)) {
+	let ret = key;
+
+	if (key === 'reset' || key === 'hidden' || key === 'grey') {
+		continue;
+	}
+
+	if (/^bg[^B]/.test(key)) {
+		ret = chalk.black(ret);
+	}
+
+	process.stdout.write(chalk[key](ret) + ' ');
+}
