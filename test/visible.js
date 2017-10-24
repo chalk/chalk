ede310303b9893146bd7cc24261a50e3b47c633a
Josh Junon <junon@zeit.co>
2017-10-23 19:53:26 -0700

    add failing test for .visible bug

--- test/visible.js
+++ test/visible.js
@@ -22,3 +22,26 @@ test('visible: no output when level is too low', t => {
 	t.is(ctx.visible.red('foo'), '');
 	t.is(ctx.red.visible('foo'), '');
 });
+
+test('test switching back and forth between enabled and disabled', t => {
+	const ctx = new m.constructor({level: 3, enabled: true});
+	t.is(ctx.red('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.visible.red('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.red.visible('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.visible('foo'), 'foo');
+	t.is(ctx.red('foo'), '\u001B[31mfoo\u001B[39m');
+
+	ctx.enabled = false;
+	t.is(ctx.red('foo'), 'foo');
+	t.is(ctx.visible('foo'), '');
+	t.is(ctx.visible.red('foo'), '');
+	t.is(ctx.red.visible('foo'), '');
+	t.is(ctx.red('foo'), 'foo');
+
+	ctx.enabled = true;
+	t.is(ctx.red('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.visible.red('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.red.visible('foo'), '\u001B[31mfoo\u001B[39m');
+	t.is(ctx.visible('foo'), 'foo');
+	t.is(ctx.red('foo'), '\u001B[31mfoo\u001B[39m');
+});
