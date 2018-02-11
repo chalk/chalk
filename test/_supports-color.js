678152cf1952f3afe1e523a306c0413ad57c34b3
Kayla Washburn <partheseas@gmail.com>
2018-02-11 02:59:24 -0700

    Update chalk to use the latest updates to supports-color (#247)

--- test/_supports-color.js
+++ test/_supports-color.js
@@ -4,10 +4,12 @@ const resolveFrom = require('resolve-from');
 module.exports = dir => {
 	require.cache[resolveFrom(dir, 'supports-color')] = {
 		exports: {
-			level: 3,
-			hasBasic: true,
-			has256: true,
-			has16m: true
+			stdout: {
+				level: 3,
+				hasBasic: true,
+				has256: true,
+				has16m: true
+			}
 		}
 	};
 };
