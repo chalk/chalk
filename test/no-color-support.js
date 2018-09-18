6a14c58e54809f15889830eddee1ea8a6acb1b2e
Chris Harwood <nazrhyn@gmail.com>
2018-09-18 04:48:47 -0400

    Add failing test for #234 (#235)

---
+++ test/no-color-support.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+
+// Spoof supports-color
+require('./_supports-color')(__dirname, {
+	level: 0,
+	hasBasic: false,
+	has256: false,
+	has16m: false
+});
+
+const chalk = require('..');
+
+test.failing('colors can be forced by using chalk.enabled', t => {
+	chalk.enabled = true;
+	t.is(chalk.green('hello'), '\u001B[32mhello\u001B[39m');
+});
