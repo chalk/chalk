98628d9f087a5c1771651fa7b867dcd2356c2123
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-28 04:10:44 +0000

    Use CommonJS-compatible export in TypeScript definition (#344)

--- index.js
+++ index.js
@@ -199,4 +199,3 @@ Object.defineProperties(Chalk.prototype, styles);
 
 module.exports = Chalk(); // eslint-disable-line new-cap
 module.exports.supportsColor = stdoutColor;
-module.exports.default = module.exports; // For TypeScript
