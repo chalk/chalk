487bb4a6fc98d90a8454568f0752e9c1313ca9f0
Mark Pedrotti <pedrottimark@gmail.com>
2019-10-07 10:01:08 -0400

    Replace plural with singular in comment

--- index.d.ts
+++ index.d.ts
@@ -214,7 +214,7 @@ declare namespace chalk {
 		hwb(hue: number, whiteness: number, blackness: number): Chalk;
 
 		/**
-		Use 8-bit unsigned numbers to set text color.
+		Use 8-bit unsigned number to set text color.
 		*/
 		ansi256(index: number): Chalk;
 
@@ -267,7 +267,7 @@ declare namespace chalk {
 		bgHwb(hue: number, whiteness: number, blackness: number): Chalk;
 
 		/**
-		Use 8-bit unsigned numbers to set background color.
+		Use 8-bit unsigned number to set background color.
 		*/
 		bgAnsi256(index: number): Chalk;
 
