bf8a5b290fbced60e3e41adb21caff78497aaf5e
silverwind <me@silverwind.io>
2023-06-28 19:31:37 +0200

    Update `supports-color` to 9.4.0
    Produced via `cp ../supports-color/{index,browser}.{js,d.ts} source/vendor/supports-color`

--- source/vendor/supports-color/index.d.ts
+++ source/vendor/supports-color/index.d.ts
@@ -1,13 +1,13 @@
 import type {WriteStream} from 'node:tty';
 
-export type Options = {
+export interface Options {
 	/**
 	Whether `process.argv` should be sniffed for `--color` and `--no-color` flags.
 
 	@default true
 	*/
 	readonly sniffFlags?: boolean;
-};
+}
 
 /**
 Levels:
@@ -21,7 +21,7 @@ export type ColorSupportLevel = 0 | 1 | 2 | 3;
 /**
 Detect whether the terminal supports color.
 */
-export type ColorSupport = {
+export interface ColorSupport {
 	/**
 	The color level.
 	*/
@@ -41,7 +41,7 @@ export type ColorSupport = {
 	Whether Truecolor 16 million colors are supported.
 	*/
 	has16m: boolean;
-};
+}
 
 export type ColorInfo = ColorSupport | false;
 
